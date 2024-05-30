0b03578673f53861f8fa9df936552c159084cccf
Fisstouk <90407928+Fisstouk@users.noreply.github.com>
2024-05-30 12:38:56 +0200

    feat: add subnet to VPC

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/28"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
