b67f230ebde5ca8517fc009f0642e556fd8c3258
Mathis <mathis.thouvenin@orange.fr>
2024-05-30 12:24:57 +0200

    Add resource aws_subnet public and private

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -3,5 +3,27 @@ resource "aws_vpc" "main" {
 
  tags = {
    Name = "Groupe1 5SI1 VPC"
+ }
 }
+
+resource "aws_subnet" "public_subnets" {
+ count              = length(var.public_subnet_cidrs)
+ vpc_id             = aws_vpc.main.id
+ cidr_block         = element(var.public_subnet_cidrs, count.index)
+ availability_zone  = element(var.azs, count.index)
+
+ tags = {
+  Name = "Public Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_subnet" "private_subnets" {
+ count              = length(var.private_subnet_cidrs)
+ vpc_id             = aws_vpc.main.id
+ cidr_block         = element(var.private_subnet_cidrs, count.index)
+ availability_zone  = element(var.azs, count.index)
+
+ tags = {
+  Name = "Private Subnet ${count.index + 1}"
+ }
 }
