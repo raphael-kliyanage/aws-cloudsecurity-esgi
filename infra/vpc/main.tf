8dbd9c761208d613988cb2284700f954be967861
Mathis <mathis.thouvenin@orange.fr>
2024-05-30 15:07:41 +0200

    Add nat-eip

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -41,14 +41,21 @@ resource "aws_route_table" "public-route-table" {
   }
  }
 
- resource "aws_route" "public-route" {
+resource "aws_route" "public-route" {
   route_table_id         = aws_route_table.public-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main-igw.id
-  }
+ }
  
- resource "aws_route_table_association" "public-subnet-association" {
+resource "aws_route_table_association" "public-subnet-association" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.public-route-table.id
-  }
- 
+ }
+
+resource "aws_eip" "nat-eip" {
+ vpc = true
+
+ tags = {
+  Name = "nat-eip"
+ }
+}
