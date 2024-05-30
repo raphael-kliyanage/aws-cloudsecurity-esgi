452220b8eae9163cb16f047ab9bf8a9e350a54a1
Mathis <mathis.thouvenin@orange.fr>
2024-05-30 15:13:33 +0200

    Add web-sg

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -68,3 +68,26 @@ resource "aws_nat_gateway" "nat-gateway" {
   Name = "nat-gateway"
  }
 }
+
+resource "aws_security_group" "web-sg" {
+ vpc_id = aws_vpc.main-vpc.id
+ name   = "web-sg"
+
+ ingress {
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ tag = {
+  Name = "web-sg"
+ }
+} 
