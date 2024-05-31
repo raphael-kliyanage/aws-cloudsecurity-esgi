d094b941ac07d832ee0d9e0ce51c86e1033ac219
raphael-kliyanage <raphael.kliyanage@gmail.com>
2024-05-31 16:40:21 +0200

    fix(cloudwatch stream): remove cloudwatch log streams

--- infra/cloudwatch/main.tf
+++ infra/cloudwatch/main.tf
@@ -25,19 +25,21 @@ resource "aws_cloudwatch_metric_alarm" "cloudwatch-s3-network" {
   insufficient_data_actions = []
 }
 
-resource "aws_cloudwatch_metric_stream" "cpu-and-network-in" {
-  name          = "CPU & Networking incoming traffic"
-  role_arn      = aws_iam_role.metric_stream_to_firehose.arn
-  firehose_arn  = aws_kinesis_firehose_delivery_stream.s3_stream.arn
-  output_format = "json"
-
-  include_filter {
-    namespace    = "AWS/EC2"
-    metric_names = ["CPUUtilization", "NetworkIn"]
-  }
-
-  include_filter {
-    namespace    = "AWS/EBS"
-    metric_names = []
-  }
-}
+# create a logging stream cpu & network incoming traffic
+# not working yet
+#resource "aws_cloudwatch_metric_stream" "cpu-and-network-in" {
+#  name          = "CPU & Networking incoming traffic"
+#  role_arn      = aws_iam_role.metric_stream_to_firehose.arn
+#  firehose_arn  = aws_kinesis_firehose_delivery_stream.s3_stream.arn
+#  output_format = "json"
+#
+#  include_filter {
+#    namespace    = "AWS/EC2"
+#    metric_names = ["CPUUtilization", "NetworkIn"]
+#  }
+#
+#  include_filter {
+#    namespace    = "AWS/EBS"
+#    metric_names = []
+#  }
+#}
