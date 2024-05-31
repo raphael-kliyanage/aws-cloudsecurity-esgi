c5030241eaa237df69ac47b96a5eaffaee7e5221
Mathis <mathis.thouvenin@orange.fr>
2024-05-31 11:12:58 +0200

    Update main.tf in vpc directory

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public-subnet" {
  availability_zone  = "eu-west-3a"
 
  tags = {
-  Name = "Public Subnet"
+  Name = "${var.public_subnet}"
  }
 }
 
@@ -22,14 +22,14 @@ resource "aws_subnet" "private-subnet" {
  availability_zone  = "eu-west-3a"
 
  tags = {
-  Name = "Private Subnet"
+  Name = "${var.private_subnet}"
  }
 
 resource "aws_internet_gateway" "main-igw" {
  vpc_id = aws_vpc.main-vpc.id
 
  tags   = {
-  Name  = "main-vpc-IGW"
+  Name  = "${var.internet_gateway}"
   }
 }
 
@@ -37,7 +37,7 @@ resource "aws_route_table" "public-route-table" {
  vpc_id = aws_vpc.main-vpc.id
 
  tags = {
-  Name = "public-route-table"
+  Name = "${var.public_route_table}"
   }
  }
 
@@ -52,33 +52,49 @@ resource "aws_route_table_association" "public-subnet-association" {
   route_table_id = aws_route_table.public-route-table.id
  }
 
-resource "aws_eip" "nat-eip" {
- vpc = true
+#resource "aws_eip" "nat-eip" {
+# vpc = true
 
- tags = {
-  Name = "nat-eip"
- }
-}
+# tags = {
+# Name = "nat-eip"
+# }
+#}
 
-resource "aws_nat_gateway" "nat-gateway" {
- allocation_id = aws_eip.nat-eip.id
- subnet_id     = aws_subnet.public-subnet.id
+#resource "aws_nat_gateway" "nat-gateway" {
+# allocation_id = aws_eip.nat-eip.id
+# subnet_id     = aws_subnet.public-subnet.id
 
- tags = {
-  Name = "nat-gateway"
- }
-}
+# tags = {
+#  Name = "nat-gateway"
+#}
+#}
 
 resource "aws_security_group" "web-sg" {
  vpc_id = aws_vpc.main-vpc.id
  name   = "web-sg"
 
  ingress {
+  description = "allow in http requests"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
  }
+
+ ingress {
+  description = "allow in https requests"
+  from_port   = "443"
+  to_port     = "443"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+   description = "allow in ssh requests"
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
 
  egress {
   from_port   = 0
