fcb9a5c1626eb8a8d1f3ddd35ebdcaa1c1485b6e
Mathis <mathis.thouvenin@orange.fr>
2024-05-31 11:16:36 +0200

    Add variables.tf in vpc directory

---
+++ infra/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "private_subnet" {
+ type = string
+}
+
+variable "public_subnet" {
+ type = string
+}
+
+variable "internet_gateway" {
+ type = string
+}
+
+variable "public_route_table" {
+ type = string
+}
